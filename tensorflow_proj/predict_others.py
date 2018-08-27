c24728891d51dbc598b5e7e3243893bd64a0f7e4
z003w5sa <z003w5sa@MD1N40UC.ad005.onehc.net>
2018-08-27 09:52:54 +0200

    update

--- tensorflow_proj/predict_others.py
+++ tensorflow_proj/predict_others.py
@@ -13,8 +13,8 @@ filename = dir_path +'/' +image_path
 image_size=128
 num_channels=3
 images = []
-
-        #READ AND RESIZE IMAGE USING OPENCV
+        
+        #PREPROCESSING: READ AND RESIZE IMAGE USING OPENCV
 # Reading the image using OpenCV
 image = cv2.imread(filename)
 
@@ -23,7 +23,7 @@ image = cv2.resize(image, (image_size, image_size),0,0, cv2.INTER_LINEAR)
 images.append(image)
 images = np.array(images, dtype=np.uint8)
 images = images.astype('float32')
-images = np.multiply(images, 1.0/255.0) 
+images = np.multiply(images, 1.0/255.0)     #Convert to RGB
 
 #The input to the network is of shape [None, image_size, image_size, num_channels]. Hence we reshape.
 x_batch = images.reshape(1, image_size, image_size, num_channels)
@@ -31,3 +31,28 @@ x_batch = images.reshape(1, image_size, image_size, num_channels)
 #Now, restore saved trained model
 sess = tf.Session()
 
+#Step 1: Recreate the network graph
+saver = tf.train.import_meta_graph('dogs-cats-model.meta')
+
+#Step 2: Now load the weights saved using the restore method
+saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+#Accessing the default graph which we have restored
+graph = tf.get_default_graph()
+
+#Now, let's get hold of he operation that we can process to get the output.
+#In the original network, y_pred is the tensor that is the prediction of the network
+y_pred = graph.get_tensor_by_name("y_pred:0")
+
+#Feed image to the input placeholders
+x = graph.get_tensor_by_name("x:0")
+y_true = graph.get_tensor_by_name("y_true:0")
+y_test_images = np.zeros((1, len(os.listdir('training_data'))))
+
+
+#Create the feed_dict required to be fed to calculate new y_pred
+feed_dict_testing = {x: x_batch, y_true:y_test_images}
+result = sess.run(y_pred, feed_dict=feed_dict_testing)
+
+#result is of this format: [probability_of_sunflower]
+print(result)
